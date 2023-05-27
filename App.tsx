9c14f014c8c45c5d3da0115d4e2f4b657d738dad
Praisecodes <cmancoder@gmail.com>
2023-05-27 12:46:04 +0100

    [Fix] Fix context bug

--- App.tsx
+++ App.tsx
@@ -3,30 +3,12 @@ import { useEffect, useState, useContext } from "react";
 import { useFonts } from "expo-font";
 import { Onboarding, MainNavs } from "./navigators";
 import { AppContext, AppContextProvider } from "./contexts";
+import MainApp from './main_app';
 
 export default function App() {
-  // const [opened, setOpened] = useState<boolean>(false);
-  const { opened, setOpened } = useContext(AppContext);
-
-  const checkDevice = async (): Promise<any> => {
-    try {
-      const val = await AsyncStorage.getItem('opened');
-      if (val !== null) { setOpened(true); return true; }
-      return false;
-    } catch (error) {
-      console.error(error);
-      console.log(opened)
-      return false;
-    }
-  }
-
-  useEffect(() => {
-    if (!checkDevice()) console.log("Error with async storage");
-  })
-
   return (
     <AppContextProvider>
-      {(!opened) ? <Onboarding /> : <MainNavs />}
+      <MainApp />
     </AppContextProvider>
   );
 }
---
+++ main_app.tsx
@@ -0,0 +1,31 @@
+import { useContext, useEffect } from "react";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { Onboarding, MainNavs } from "./navigators";
+import { AppContext } from "./contexts";
+
+const MainApp = (): JSX.Element => {
+
+    const { opened, setOpened } = useContext(AppContext);
+
+    const checkDevice = async (): Promise<any> => {
+        try {
+            const val = await AsyncStorage.getItem('opened');
+            if (val !== null) { setOpened(true); return true; }
+            return false;
+        } catch (error) {
+            console.error(error);
+            console.log(opened)
+            return false;
+        }
+    }
+
+    useEffect(() => {
+        if (!checkDevice()) console.log("Error with async storage");
+    })
+
+    return (
+        (!opened) ? <Onboarding /> : <MainNavs />
+    );
+}
+
+export default MainApp;
